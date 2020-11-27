89c036c74e41a85274fbf61fa12b1242aeddc166
Miguel Aquino <jcaquino0945@gmail.com>
2020-11-27 15:21:24 +0800

    dded user and retrieval of code

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    'secretKey': '12345-67890-09876-54321',
+    'mongoUrl' : 'mongodb+srv://jcaquino0945:hotdog123@frontcluster.r6iq9.mongodb.net/seTrialDb?retryWrites=true&w=majority'
+}
