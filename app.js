e9e44ce86427b900b432366bcb12b9d33e73fd10
Miguel Aquino <jcaquino0945@gmail.com>
2020-11-27 15:25:52 +0800

    dded user and retrieval of code 2

--- app.js
+++ app.js
@@ -3,11 +3,25 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var passport = require('passport');
+var cors = require('cors');
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+const mongoose = require('mongoose');
+var config = require('./config');
+
+const url = config.mongoUrl;
+const connect = mongoose.connect(url);
+
+connect.then((db) => {
+  console.log("Connected correctly to server");
+}, (err) => { console.log(err); });
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -16,13 +30,43 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+/*
+app.use(session({
+  name: 'session-id',
+  secret: '12345-67890-09876-54321',
+  saveUninitialized: false,
+  resave: false,
+  store: new FileStore()
+}));
+*/
+app.use(passport.initialize());
+app.use(cors());
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', indexRouter);
-
 app.use('/users', usersRouter);
 
+app.use(express.static(path.join(__dirname, 'public')));
+/*
+function auth (req, res, next) {
+  console.log(req.user);
+
+  if (!req.user) {
+    var err = new Error('You are not authenticated!');
+    err.status = 403;
+    next(err);
+  }
+  else {
+        next();
+  }
+}
+app.use(auth);
+
+app.get('/admin', function(req, res, next) {
+  res.send('respond with a admin');
+});
+*/
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
